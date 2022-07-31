87857ac48a9b2ed2ed9bdf08a16ee89ec7de5277
JobairulHassan <hasanjobairul@gmail.com>
2022-07-31 20:02:18 +0600

    Add project files.

---
+++ Controllers/RecruiterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiredHunters.Controllers
+{
+    public class RecruiterController : Controller
+    {
+        // GET: Recruiter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
